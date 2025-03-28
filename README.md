b050b949c99b1c16afe2c0c06e4783d028135901
camdenblatchly <camdenblatchly@gmail.com>
2025-03-28 16:42:10 -0400

    added support for s3 and tract definitions

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Package for easily accessing and manipulating federal rural definitions
 # install.packages("devtools")
 devtools::install_github("ruralinnovation/ruraldefinitions")
 ```
+
+## Getting started
+
+County-level rural definitions are loaded into the package and can be directly 
+accessed using `ruraldefinitions::cbsa_2020`. Tract-level definitions and 
+lower can be accessed using the `get_definition` function which requires a 
+definition name and year as parameters. For instance, the 
+2020 CORI definition can be accessed with with `get_definition("cori", 2020)`.
+
+Here are the definitions and years that are currently supported by the 
+package:
+
+| Name          | Year | Geographic Unit |
+| ------------- | ---- | --------------- |
+| RUCA          | 2010 | Tract           |
+| CORI          | 2020 | Tract           |
+| CBSA          | 2020 | County          |
+
+
---
+++ data-raw/cori_2020.R
@@ -0,0 +1,39 @@
+library(tigris)
+library(dplyr)
+library(sf)
+library(readxl)
+library(curl)
+library(here)
+library(arrow)
+library(cori.db)
+
+i_am("data-raw/cori_2020.R")
+
+con <- connect_to_db("core_data")
+ruca_raw <- read_db(con, "rural_definitions_tract_2020")
+DBI::dbDisconnect(con)
+
+ruca_clean <- ruca_raw %>%
+  mutate(
+    name = "RUCA",
+    year = 2020,
+    is_rural = ifelse(
+      rural_def == 1,
+      "Rural",
+      "Nonrural"
+    ),
+    rural_def = ifelse(
+      rural_cbsa_2019 == "Nonmetro",
+      "Nonmetro county",
+      ifelse(
+        rural_cbsa_2019 == "Metro" & rural_ruca_flag_2020 == 1,
+        "Rural tract in metro county",
+        "Metro county"
+      )
+    )
+  ) %>%
+  select(geoid, name, year, rural_def, is_rural)
+
+parquet_buffer <- tempfile()
+write_parquet(ruca_clean, parquet_buffer)
+cori.db::put_s3_object("ruraldefinitions", "clean/cori_2020.parquet", file_path = parquet_buffer)
